8103449dab36ccc678b62ff595dce05295ac4a12
Rashi Ratan <rashiiratan@gmail.com>
2024-10-06 10:41:49 -0400

    Create and manage departments and employees

--- employee_management.js
+++ employee_management.js
@@ -43,3 +43,26 @@ class Manager extends Employee{
         console.log(`Employee's name: ${this.name}\n Position: ${this.position} \n Salary: $${this.salary} \n Bonus: $${this.bonus}`); 
     }
 }
+
+//task 5
+
+// Create departments
+const sales = new Department("Sales");
+const finance = new Department("Finance");
+// Create employees
+const alyssa = new Employee("Alyssa", 87000, "Sales Person", "Sales");
+const barbara = new Employee("Barbara", 78500, "Analyst", "Finance");
+const charlotte = new Manager("Charlotte", 145000, "Sales Manager", "Sales", 50000);
+const denis = new Manager("Denis", 157000, "Finance Manager", "Finance", 30000);
+
+ // Add employees to departments
+sales.addEmployee(alyssa);
+sales.addEmployee(charlotte);
+finance.addEmployee(barbara);
+finance.addEmployee(denis);
+
+ // Calculate total salary for each department
+console.log(`Total salary for Sales: $${sales.getDepartmentSalary()}`);
+console.log(`Total salary with bonuses for Sales: $${sales.calculateTotalSalaryWithBonus()}`);
+console.log(`Total salary for Finance: $${finance.getDepartmentSalary()}`);
+console.log(`Total salary with bonuses for Finance: $${finance.calculateTotalSalaryWithBonus()}`);
